5d5c01bcb0f8b2278f5da241c83a200b3523fc31
abubakar <abubakarfarah011@gmail.com>
2026-01-05 22:01:18 +0300

    first commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,22 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  if (string.length === 0) return result;
+
+  for (let start = 0; start < string.length; start++) {
+    for (let end = start +1; end <= string.length; end++) {
+      const substring = string.substring(start, end);
+  if (substring.length > 0) {
+     result.push(substring);
+     }
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
